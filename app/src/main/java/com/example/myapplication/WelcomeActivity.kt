b8bc80f9bed7411055018da759350cea71cf6156
raffitri <114563097+raffitri@users.noreply.github.com>
2024-11-25 19:10:40 +0700

    bagian Raffitri

---
+++ app/src/main/java/com/example/myapplication/WelcomeActivity.kt
@@ -0,0 +1,104 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun WelcomeActivity (navController: NavController) {
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.tepaspapandayanhd),
+            contentDescription = "BackgroundWelcome",
+            Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.38f) // Bagian bawah layar (25%)
+                .align(Alignment.BottomCenter), // Posisi di bawah layar
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center // Rata tengah secara vertikal
+        ) {
+
+            Button(
+                onClick = {
+                    navController.navigate("register") // Arahkan ke halaman Login
+                },
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+                    .width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF6400)),
+                elevation = ButtonDefaults.elevatedButtonElevation(
+                    defaultElevation = 8.dp,
+                    pressedElevation = 12.dp
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(
+                    stringResource(id = R.string.createAccount), color = Color(0xFFFFFFFF),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W800
+                )
+            }
+
+            Button(
+                onClick = {
+                    navController.navigate("login") // Arahkan ke halaman Register
+                },
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+                    .width(200.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF6400)),
+                elevation = ButtonDefaults.elevatedButtonElevation(
+                    defaultElevation = 8.dp,
+                    pressedElevation = 12.dp
+                ),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(
+                    stringResource(id = R.string.login), color = Color(0xFFFFFFFF),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W800
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_3A)
+@Composable
+private fun RegisterScreenPreview() {
+    MaterialTheme {
+        WelcomeActivity(navController = rememberNavController())
+    }
+}
